4318863f99d79eb463aff44caff18d7f6a113646
cedric <72254383+faille@users.noreply.github.com>
2020-10-08 09:18:17 +0200

    Delete oxo2.py

--- oxo2.py
+++
@@ -1,9 +0,0 @@
-import serial
-
-port = "COM7"
-conn = serial.Serial(port,115200)
-
-while True:
-    xy = input("Donne un x : ") + input("Donne un y : ")
-    print(xy)
-    conn.write(xy.encode())
