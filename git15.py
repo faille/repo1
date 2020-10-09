3ca8fc009e7f67e4c2a671c48c4402f871b5cdd8
cedric <72254383+faille@users.noreply.github.com>
2020-10-09 17:34:10 +0200

    Update git15.py

--- git15.py
+++ git15.py
@@ -1,23 +1,21 @@
 from microbit import *
 
 def lumiere(row):
-    for i in range(row):
-        for i in range(5):
-            display.set_pixel(row-1, i, 9)
+    for i in range(5):
+        display.set_pixel(row, i, 9)
 
 while True:
-    #display.scroll(display.read_light_level())
     intensity = display.read_light_level()
     #display.scroll(intensity)
     if intensity >= 0 and intensity <= 50:
-        lumiere(1)
+        lumiere(0)
     if intensity > 50 and intensity <= 100:
-        lumiere(2)
+        lumiere(1)
     if intensity > 100 and intensity <= 150:
-        lumiere(3)
+        lumiere(2)
     if intensity > 150 and intensity <= 200:
-        lumiere(4)
+        lumiere(3)
     if intensity > 200 and intensity <= 250:
-        lumiere(5)
-    sleep(2000)
+        lumiere(4)
+    sleep(1000)
     display.clear()
